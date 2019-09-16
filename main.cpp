dd596b2053b372e77c9308fdee5834f894740a53
karennik98 <karen.nikoghosyan.98@gmail.com>
2019-09-16 14:16:19 +0400

    Added some test cases

--- main.cpp
+++ main.cpp
@@ -9,6 +9,12 @@ int main()
         HugeInt num2("-10");
         HugeInt num3("100000000");
         HugeInt num4("-2");
+
+        // HugeInt num1("500000000000000000000000000000");
+        // HugeInt num2("-165964646464646464646464646400");
+        // HugeInt num3("106464645464665465465460000000");
+        // HugeInt num4("-265995995995999962134645455440");
+
         std::cout<<"num1: "<<num1<<std::endl;
         std::cout<<"num2: "<<num2<<std::endl;
         std::cout<<"num3: "<<num3<<std::endl;
@@ -24,6 +30,7 @@ int main()
         std::cout<<"num1 * num2 = ";
         std::cout<<num1 * num2<<std::endl;
 
+        // Divide operator work slowly its need to optimize: TODO
         std::cout<<"num1 / num2 = ";
         std::cout<<num1 / num2<<std::endl;
 
